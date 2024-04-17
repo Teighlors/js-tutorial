baf8f2f5aacd62bc7c6677542f551c9b8884860e
Teighlors <126301686+Teighlors@users.noreply.github.com>
2024-04-16 19:26:01 -0500

    update

--- events.js
+++ events.js
@@ -3,3 +3,18 @@ var button = document.getElementById("my-button");
 button.addEventListener("click", function() {
     alert("Button was clicked");
 });
+
+
+function mouseOver() {
+    document.getElementById("my-button").style.borderColor = "green";
+    document.getElementById("my-button").style.color = "green";
+    document.getElementById("my-button").style.boxShadow = "5px 5px 5px 5px green";
+
+}
+
+function mouseOut() {
+    document.getElementById("my-button").style.borderColor = "white";
+    document.getElementById("my-button").style.color = "white";
+    document.getElementById("my-button").style.boxShadow = "none";
+
+}
